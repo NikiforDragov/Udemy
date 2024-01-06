7919f153d16008a8bddfa89c98dfd8b3fee14fc2
Nikifor Dragov <ndragov99@gmail.com>
2024-01-06 14:49:21 +0200

    add

--- Web Development Projects/16. DOM/DOM Challenge Starting Files/index.js
+++ Web Development Projects/16. DOM/DOM Challenge Starting Files/index.js
@@ -1,2 +1,4 @@
 document.querySelector('ul li:nth-of-type(3)').innerHTML = 'Nikifor';
 document.querySelector('.list a').style.color = 'red';
+document.querySelector('#btn').style.backgroundColor = 'yellow';
+document.querySelector('h1').classList.add('huge');
