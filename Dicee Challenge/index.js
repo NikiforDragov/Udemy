43cb6920847edfcb1f14f66f8beb0fe9037c2414
Nikifor Dragov <ndragov99@gmail.com>
2024-01-08 16:40:49 +0200

    Add random dice

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,5 +1,6 @@
 // Create random number from 1 to 6
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
+document.querySelector('.img1').setAttribute('src', `./images/dice${randomNumber1}.png`);
 
     
