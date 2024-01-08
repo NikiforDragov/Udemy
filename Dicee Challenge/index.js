20add53c07fc01d1dc8639e4f8a2a1a283c82c55
Nikifor Dragov <ndragov99@gmail.com>
2024-01-08 16:45:57 +0200

    Display result

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,6 +1,24 @@
 // Create random number from 1 to 6
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('.img1').setAttribute('src', `./images/dice${randomNumber1}.png`);
+// Set dice image;
+document
+    .querySelector('.img1')
+    .setAttribute('src', `./images/dice${randomNumber1}.png`);
+document
+    .querySelector('.img2')
+    .setAttribute('src', `./images/dice${randomNumber2}.png`);
 
-    
+let result = '';
+
+if (randomNumber1 > randomNumber2) {
+    result = 'Player 1 Wins!';
+} else if (randomNumber1 < randomNumber2) {
+    result = 'Player 2 Wins!';
+} else {
+    result = 'Draw!';
+}
+
+// Set correct result
+document.querySelector('h1').textContent = result;
