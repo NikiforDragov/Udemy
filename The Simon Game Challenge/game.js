42075a4bbaa1df5b55d58bfa7e433fe351d965d6
Nikifor Dragov <ndragov99@gmail.com>
2024-02-24 15:55:05 +0200

    Create user pattern by click

--- The Simon Game Challenge/game.js
+++ The Simon Game Challenge/game.js
@@ -1,19 +1,24 @@
-const buttonColors = ['red', 'blue', 'green', 'yellow']
+const buttonColors = ['red', 'blue', 'green', 'yellow'];
 
 const gamePattern = [];
 
+const userClickedPattern = [];
+
+$('.btn').on('click', (e) => {
+    const userChosenColor = e.currentTarget.id;
+    userClickedPattern.push(userChosenColor);
+});
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
 
     let randomChosenColor = buttonColors[randomNumber];
-
     gamePattern.push(randomChosenColor);
 
-    const audio = new Audio(`sounds/${randomChosenColor}.mp3`)
-
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeIn(100);
-    audio.play()
 
+    const audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+    audio.play();
 }
+
 
