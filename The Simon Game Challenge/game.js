ba7970abf8e8d9d6e89e4e5596a458976dd0f420
Nikifor Dragov <ndragov99@gmail.com>
2024-02-24 15:19:08 +0200

    Add random color to the pattern

---
+++ The Simon Game Challenge/game.js
@@ -0,0 +1,12 @@
+const buttonColors = ['red', 'blue', 'green', 'yellow']
+
+const gamePattern = [];
+
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+
+    let randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+}
