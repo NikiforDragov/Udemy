4c528e54f4ce3be832d1d62954309e32343ae6af
Nikifor Dragov <ndragov99@gmail.com>
2024-02-24 16:16:18 +0200

    Add pressing animation

--- The Simon Game Challenge/game.js
+++ The Simon Game Challenge/game.js
@@ -8,6 +8,8 @@ $('.btn').on('click', (e) => {
     userClickedPattern.push(userChosenColor);
 
     playSound(userChosenColor);
+
+    animatePress(userChosenColor);
 });
 
 function nextSequence() {
@@ -19,9 +21,16 @@ function nextSequence() {
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100);
 
     playSound(randomChosenColor);
-}
+};
 
 function playSound(name) {
     const audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
+};
+
+function animatePress(currentColor) {
+    $(`#${currentColor}`).addClass('pressed');
+    setTimeout(() => {
+        $(`#${currentColor}`).removeClass('pressed');
+    }, 100);
 }
