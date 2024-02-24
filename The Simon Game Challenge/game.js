15f1cdfc9b24acda045c106b7458a7eaebf6da81
Nikifor Dragov <ndragov99@gmail.com>
2024-02-24 16:38:56 +0200

    Add level

--- The Simon Game Challenge/game.js
+++ The Simon Game Challenge/game.js
@@ -3,6 +3,20 @@ const buttonColors = ['red', 'blue', 'green', 'yellow'];
 const gamePattern = [];
 const userClickedPattern = [];
 
+let isGameStarted = false;
+
+let level = 0;
+
+$(document).keydown(() => {
+    if (!isGameStarted) {
+
+        $('#level-title').text(`Level ${level}`);
+
+        nextSequence();
+        isGameStarted = true;
+    }
+});
+
 $('.btn').on('click', (e) => {
     const userChosenColor = e.currentTarget.id;
     userClickedPattern.push(userChosenColor);
@@ -13,20 +27,24 @@ $('.btn').on('click', (e) => {
 });
 
 function nextSequence() {
+    level++;
+
+    $('#level-title').text(`Level ${level}`);
+
     let randomNumber = Math.floor(Math.random() * 4);
 
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).fadeOut(100);
+    $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenColor);
-};
+}
 
 function playSound(name) {
     const audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
-};
+}
 
 function animatePress(currentColor) {
     $(`#${currentColor}`).addClass('pressed');
