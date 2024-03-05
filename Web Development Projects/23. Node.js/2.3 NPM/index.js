a7748fa9fab1e1c74f0c4b6dd0c8990d979bb984
Nikifor Dragov <ndragov99@gmail.com>
2024-03-05 20:51:25 +0200

    Add

---
+++ Web Development Projects/23. Node.js/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from 'sillyname';
+let sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import superheroes from 'superheroes';
+let mySuperHero = superheroes.random();
+
+console.log(`I am ${mySuperHero}`);
